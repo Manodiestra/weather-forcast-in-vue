9c1d40ef64309647f53b76edf17b254fd47b24b1
elijah_blake <elijahdarrelblake@gmail.com>
2019-04-08 10:14:48 -0700

    Added the initial files and wrote the html file

---
+++ src/vue.js
@@ -0,0 +1,11 @@
+apilayer key e930d5648454273dc89327a525966b27
+OWM key fd8389fcd4b723879831f6cb0f09c2b2
+
+var app = new vue ({
+        el: '#middle',
+        data : {
+            node: false,
+        }
+        computed: {
+            //functions
+        }
